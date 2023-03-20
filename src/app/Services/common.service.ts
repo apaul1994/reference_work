c8a43b383431c0b3f9e27c6bef906ae84b955902
apaul1994 <ananga24@gmail.com>
2023-03-20 17:07:18 +0530

    ag-grid

--- src/app/Services/common.service.ts
+++ src/app/Services/common.service.ts
@@ -8,99 +8,13 @@ import { HttpClient } from '@angular/common/http';
 export class CommonService {
 
   constructor(public http : HttpClient) { }
-  dummyData = of([
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 },
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 },
-    { make: 'asf', model: 'gasd', price: 874100 },
-    { make: 'Poryertsche', model: 'tww', price: 82000 }
-  ]);
-  getDummyData(){
-
-    return this.dummyData;
-  }
 
   getInterval(){
     return interval(1000);
   }
 
   getDummyPost(){
-    return this.http.get('https://www.ag-grid.com/example-assets/olympic-winners.json');
-    // return this.http.get('https://jsonplaceholder.typicode.com/posts');
+    // return this.http.get<UsedObject[]>('https://www.ag-grid.com/example-assets/olympic-winners.json');
+    return this.http.get('https://jsonplaceholder.typicode.com/posts');
   }
 }
--- src/app/ag-grid/ag-grid.component.ts
+++ src/app/ag-grid/ag-grid.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { delay, Subscription, tap } from 'rxjs';
+import { Component, HostListener, Input, ViewChild } from '@angular/core';
+import { IGetRowsParams } from 'ag-grid-community';
+import { filter, of } from 'rxjs';
 import { CommonService } from '../Services/common.service';
 
 @Component({
@@ -7,55 +8,91 @@ import { CommonService } from '../Services/common.service';
   templateUrl: './ag-grid.component.html',
   styleUrls: ['./ag-grid.component.scss']
 })
-export class AgGridComponent implements OnInit, OnDestroy{
-  constructor(private service:CommonService){}
-  
-  public rowData:any;
-  public postData:any;
+export class AgGridComponent{
+  public columnDefs: any[];
+  public rowData!: any[];
+  public gridOptions: any;
+  public info!: string;
+  private totalRows = 100;
+  private dataLatencyInMilliSeconds = 2000;
+  public rowHeight = 30;
+  public rows:any;
 
-  ngOnInit(): void {
-    console.log("Inside OnInit");
-    this.service.getDummyData().subscribe((data)=>{
-      this.rowData=data;
-    });
+  constructor(public service:CommonService) {
+    const checkBoxColumDefn = {
+      headerName: 'Select',
+      field: '',
+      flex: 0.1,
+      filter: false,
+      checkboxSelection: true,
+      suppressSizeToFit: true,
+      cellStyle: (params:any) => {
+        if (params.data?.qtyAlloc > 0) {
+          params.node.selected = true;
+          params.node.selectable = true;
+          return { 'pointer-events': 'none', opacity: '0.7', padding: '' };
+        } else {
+          params.node.selectable = true;
+          return { padding: '' };
+        }
+      },
+    };
+    const col4 = { headerName: 'id ', field: 'id' };
+    const col5 = { headerName: 'userId', field: 'userId' };
+    const col6 = { headerName: 'title', field: 'title' };
 
-    this.service.getDummyPost().pipe(delay(2000))
-    .subscribe((data)=>{
-      console.log(data);
-      this.postData = data;
+    this.columnDefs = [checkBoxColumDefn, col4, col5, col6];
+
+    this.gridOptions = {
+      enableCellTextSelection: true,
+      suppressMovableColumns: true,
+      suppressLoadingOverlay: false,
+      stopEditingWhenCellsLoseFocus: true,
+      suppressDragLeaveHidesColumns: false,
+      suppressHorizontalScroll: false,
+      singleClickEdit: true,
+      suppressRowClickSelection: true,
+      rowSelection: 'multiple',
+      cacheBlockSize: 20,
+      maxBlocksInCache: Infinity,
+      rowModelType: 'infinite',
+    };
+  }
+
+
+  private getRowData(startRow: number, endRow: number) {
+    this.totalRows = this.rows.length
+    console.log(this.rows.slice(startRow,endRow))
+    return of(this.rows.slice(startRow,endRow));
+  }
+
+  onGridReady(params: any) {
+    this.service.getDummyPost().subscribe((data)=>{
+      this.rows=data;
+      params.api.setDomLayout('normal');
+      params.api.setDatasource(this.getDataSource());
     })
   }
-  columnDefs = [
-    {headerName: 'Make', field: 'make' },
-		{headerName: 'Model', field: 'model' },
-		{headerName: 'Price', field: 'price'}
-	];
 
-  columnDef1 = [
-    {headerName: 'Id', field: 'id' },
-		{headerName: 'UserId', field: 'userId' },
-		{headerName: 'Title', field: 'title'}
-	];
-  columnDef2 = [
-
-    
-    {headerName: 'age', field: 'age' },
-		{headerName: 'athlete', field: 'athlete' },
-		{headerName: 'bronze', field: 'bronze'},
-		{headerName: 'country', field: 'country'},
-		{headerName: 'date', field: 'date'},
-		{headerName: 'gold', field: 'gold'},
-		{headerName: 'silver', field: 'silver'},
-		{headerName: 'sport', field: 'sport'},
-		{headerName: 'total', field: 'total'},
-		{headerName: 'year', field: 'year'},
-	];
-
-  onGridReady(event:any){
-    console.log(event)
+  private getDataSource() {
+    const dataSource = {
+      getRows: (params: IGetRowsParams) => {
+        this.info =
+          'Getting datasource rows, start: ' +
+          params.startRow +
+          ', end: ' +
+          params.endRow;
+        if (this.totalRows >= params.endRow) {
+          setTimeout(() => {
+            this.getRowData(params.startRow, params.endRow).subscribe(
+              (data:any) => {
+                params.successCallback(data, this.totalRows);
+              }
+            );
+          }, this.dataLatencyInMilliSeconds);
+        }
+      },
+    };
+    return { ...dataSource };
   }
-
-  ngOnDestroy(): void {
-  }
-
 }
